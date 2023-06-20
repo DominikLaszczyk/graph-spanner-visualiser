a2815e304d22fa8bc3467275731c0aa0bd1d1ab3
Dominik Laszczyk <dominik.laszczyk1@gmail.com>
2023-06-20 02:44:28 +0200

    removing nodes working

--- public/javascripts/graph.js
+++ public/javascripts/graph.js
@@ -1,3 +1,6 @@
+var currentModeEventListener = null;
+var nodeEventListener = null
+
 document.addEventListener("DOMContentLoaded", function() {
 
     //adjust the size and positioning of cytoscape graph based on navbar size
@@ -69,12 +72,16 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function changeMode(buttonId, cy) {
     switch(buttonId) {
+        case "btnradio0":
+            changeModeToMoveAround(cy)
+            break;
         case "btnradio1":
             console.log("mode changed to 'add nodes'")
             changeModeToAddNodes(cy)
             break;
         case "btnradio2":
             console.log("mode changed to 'remove nodes'")
+            changeModeToRemoveNodes(cy)
             break;
         case "btnradio3":
             console.log("mode changed to 'add edges'")
@@ -85,36 +92,72 @@ function changeMode(buttonId, cy) {
     } 
 }
 
-function changeModeToAddNodes(cy) {
-    //Get references to necessary elements
-    const cytoscapeDiv = document.getElementById('cy');
-    const navbar1 = document.getElementById('navbar1');
-    const navbar2 = document.getElementById('navbar2');
-    const navbarOffsetHeight = navbar1.offsetHeight + navbar2.offsetHeight
-
-    //Calculate adjusted position
-    function getAdjustedPosition(event) {
-        const rect = cytoscapeDiv.getBoundingClientRect();
-        var pan = cy.pan();
-        var distanceScrolledX = pan.x;
-        var distanceScrolledY = pan.y;
-        const zoom = cy.zoom()
-
-        const x = (event.clientX-rect.left - distanceScrolledX)/zoom;
-        const y = (event.clientY - rect.top - distanceScrolledY)/zoom;
-
-        return { x, y };
+function changeModeToMoveAround(cy) {
+    if(currentModeEventListener !== null) {
+        const cytoscapeDiv = document.getElementById('cy');
+        cytoscapeDiv.removeEventListener('click', currentModeEventListener);
     }
 
-    //Attach click event listener
-    cytoscapeDiv.addEventListener('click', function(event) {
-        const position = getAdjustedPosition(event);
+    if(cy !== null && nodeEventListener !== null) {
+        cy.off('click', 'node', nodeEventListener);
+    }
+}
 
+function changeModeToAddNodes(cy) {
+    changeModeToMoveAround(cy)
+    //Get references to necessary elements
+    // const navbar1 = document.getElementById('navbar1');
+    // const navbar2 = document.getElementById('navbar2');
+    // const navbarOffsetHeight = navbar1.offsetHeight + navbar2.offsetHeight
+
+    function addNodeEventListener(event) {
+        const position = getAdjustedCursorPosition(event, cy);
+    
         // Create a node at the adjusted position
         cy.add({
             group: 'nodes',
             data: { id: 'node-' + Date.now() },
             position: position
         });
-    });
+    }
+
+    currentModeEventListener = function(event) {
+        addNodeEventListener(event)
+    }
+
+    const cytoscapeDiv = document.getElementById('cy');
+    cytoscapeDiv.addEventListener('click', currentModeEventListener);
+}
+
+function changeModeToRemoveNodes(cy) {
+    changeModeToMoveAround(cy)
+
+    nodeEventListener = function(event) {
+        var node = event.target;
+        node.remove();
+    }
+
+    function removeNodeEventListener() {
+        cy.on('click', 'node', nodeEventListener);
+    }
+
+    currentModeEventListener = removeNodeEventListener()
+
+    const cytoscapeDiv = document.getElementById('cy');
+    cytoscapeDiv.addEventListener('click', currentModeEventListener);
+}
+
+//Calculate adjusted position
+function getAdjustedCursorPosition(event, cy) {
+    const cytoscapeDiv = document.getElementById('cy');
+    const rect = cytoscapeDiv.getBoundingClientRect();
+    var pan = cy.pan();
+    var distanceScrolledX = pan.x;
+    var distanceScrolledY = pan.y;
+    const zoom = cy.zoom()
+
+    const x = (event.clientX - rect.left - distanceScrolledX)/zoom;
+    const y = (event.clientY - rect.top - distanceScrolledY)/zoom;
+
+    return { x, y };
 }
