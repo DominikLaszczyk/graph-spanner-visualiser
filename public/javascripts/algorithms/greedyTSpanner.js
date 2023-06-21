08c163b1da7cec9b7f70b57c3ae98ae0aae9aa78
Dominik Laszczyk <dominik.laszczyk1@gmail.com>
2023-06-22 01:43:11 +0200

    greedy tspanner animations

--- public/javascripts/algorithms/greedyTSpanner.js
+++ public/javascripts/algorithms/greedyTSpanner.js
@@ -2,6 +2,9 @@ function runGreedyTSpanner(cy, cyResult, distortionFactor, layout, animationDela
     //clear result graph
     cyResult.elements().remove()
 
+    // Returns a Promise that resolves after "ms" Milliseconds
+    const timer = ms => new Promise(res => setTimeout(res, ms))
+
     // Retrieve the list of edges from the graph
     var edges = cy.edges();
 
@@ -23,42 +26,86 @@ function runGreedyTSpanner(cy, cyResult, distortionFactor, layout, animationDela
         return a.weight - b.weight;
     });
 
-    for (let i = 0; i < edgeList.length; i++) {
-        const edge = edgeList[i];
-        var source = edge.source;
-        var target = edge.target;
-        var weight = edge.weight
+    async function load () {
+        for (let i = 0; i < edgeList.length; i++) {
+            const edge = edgeList[i];
+            var source = edge.source;
+            var target = edge.target;
+            var weight = edge.weight
 
-        // Create nodes if they don't exist
-        if (!cyResult.$id(source).length) {
-            cyResult.add({ data: { id: source } });
-        }
-        if (!cyResult.$id(target).length) {
-            cyResult.add({ data: { id: target } });
-        }
+            // Create nodes if they don't exist
+            if (!cyResult.$id(source).length) {
+                cyResult.add({ data: { id: source } });
+            }
+            if (!cyResult.$id(target).length) {
+                cyResult.add({ data: { id: target } });
+            }
 
-        //calculate shortest weight path in cyResult
-        var dijkstra = cyResult.elements().dijkstra("#" + source, function(edge){
-            return edge.data('weight');
-        });
-        var shortestPathWeight = dijkstra.distanceTo(cyResult.$("#" + target));
-
-        //if greedy tspanner criteria met, add edge to cyResult
-        if ((shortestPathWeight > distortionFactor * weight) || (shortestPathWeight === Infinity)) {
-            // Create the edge
-            cyResult.add({ 
-                data: { 
-                    id: `${source}-${target}`, 
-                    source: source, 
-                    target: target,
-                    weight: weight
-                } 
+            //calculate shortest weight path in cyResult
+            var dijkstra = cyResult.elements().dijkstra("#" + source, function(edge){
+                return edge.data('weight');
             });
+            var shortestPathWeight = dijkstra.distanceTo(cyResult.$("#" + target));
 
+            //if greedy tspanner criteria met, add edge to cyResult
+            if ((shortestPathWeight > distortionFactor * weight) || (shortestPathWeight === Infinity)) {
+                // Create the edge
+                cyResult.add({ 
+                    data: { 
+                        id: `${source}-${target}`, 
+                        source: source, 
+                        target: target,
+                        weight: weight
+                    } 
+                });
+
+                if(animationDelay > 0) {
+                    const newNode1 = cyResult.$id(source);
+                    const newNode2 = cyResult.$id(target);
+                    const newEdge = cyResult.$id(source + "-" + target);
+                    newNode1.style({
+                        'background-color': '#9336d6',
+                        'border-color': '#4a0080',
+                    });
+
+                    newNode2.style({
+                        'background-color': '#9336d6',
+                        'border-color': '#4a0080',
+                    });
+                
+                    newEdge.style({
+                        'line-color': '#9336d6',
+                        'target-arrow-color': '#4a0080',
+                    });
+
+                    applyAutomaticLayout(cyResult, layout, animationDelay)
+                    await timer(animationDelay);
+
+                    newNode1.style({
+                        'background-color': '#878787',
+                        'border-color': 'black',
+                    });
+
+                    newNode2.style({
+                        'background-color': '#878787',
+                        'border-color': 'black',
+                    });
+                
+                    newEdge.style({
+                        'line-color': '#ccc',
+                        'target-arrow-color': '#ccc',
+                    });
+                }
+
+            }
+        };
+
+        if(animationDelay === 0) {
+            applyAutomaticLayout(cyResult, layout, animationDelay)
         }
-    };
+    }
 
-    applyAutomaticLayout(cyResult, layout, animationDelay)
+    load()
 
     
 }
